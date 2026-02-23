dc03911c12c4b9bff8e13f0994966e044cdecf9d
Hyndavi <hyndaviyogithakuna@gmail.com>
2026-02-23 18:43:34 +0530

    Included Dashboard

--- xyz-analytics-portal/src/pages/QualityDashboard.js
+++ xyz-analytics-portal/src/pages/QualityDashboard.js
@@ -3,14 +3,22 @@ import React from 'react';
 export default function QualityDashboard() {
   return (
     <div>
-      <h2>Quality Metrics Dashboard</h2>
+      <h1>Quality Metrics Dashboard</h1>
+      <div
+        style={{
+           height: "100vh", width: "100%", marginTop:"20px"  
+          //  ,transform: "scale(1.1)",
+      //  transformOrigin: "top center",
+        }}
+      >
       <iframe 
         title="Quality Dashboard"
-        src="https://app.powerbi.com/YOUR_QUALITY_REPORT_EMBED_URL"
+        src="https://drive.google.com/file/d/1Laijccx1eZr054mQDzYtWq5PiNzQiCON/preview"
         className="powerbi-container"
         frameBorder="0"
         allowFullScreen={true}
       />
+      </div>
     </div>
   );
 }
--- xyz-analytics-portal/src/pages/SalesDashboard.js
+++ xyz-analytics-portal/src/pages/SalesDashboard.js
@@ -3,14 +3,49 @@ import React from 'react';
 export default function SalesDashboard() {
   return (
     <div>
-      <h2>Sales Performance Dashboard</h2>
+      <h1>Sales Performance Dashboard</h1>
+      <div
+        style={{
+           height: "100vh", width: "100%", marginTop:"20px"  
+          //  ,transform: "scale(1.1)",
+      //  transformOrigin: "top center",
+        }}
+      >
       <iframe 
         title="Sales Dashboard"
-        src="https://app.powerbi.com/YOUR_SALES_REPORT_EMBED_URL"
+        src="https://drive.google.com/file/d/1vuieE7xSTP7G8aeGK5ezwPlPqszDpUHe/preview"
         className="powerbi-container"
         frameBorder="0"
         allowFullScreen={true}
       />
+      </div>
     </div>
   );
 }
+// import React from 'react';
+// import '../App.css';
+// export default function SalesDashboard() {
+//   return (
+//     <div style={{ overflow: "hidden", height: "90vh" }}>
+//       <h1>Sales Performance Dashboard</h1>
+//        <div
+//         style={{
+//            height: "100vh", width: "100%"  
+//       //      ,transform: "scale(1.625)",
+//       //  transformOrigin: "top center",
+//         }}
+//       >
+//       <iframe 
+//   title="Sales Dashboard PDF"
+//   src="https://drive.google.com/file/d/1BRUlgcqvncPB0TRL2PJKm-3To97FdnuJ/preview"
+//   className="powerbi-container"
+//   style={{
+//       width: "100%",
+//       height: "100vh",
+//       border: "none",
+//       marginTop:"30px",
+//     }}
+// /></div>
+//     </div>
+//   );
+// }
