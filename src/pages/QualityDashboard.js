9ddc53b7adeab41ea54c10aba06b08e3ca610c96
Hyndavi <hyndaviyogithakuna@gmail.com>
2026-02-25 11:03:44 +0530

    Added xyz-report files

--- src/pages/QualityDashboard.js
+++ src/pages/QualityDashboard.js
@@ -13,7 +13,7 @@ export default function QualityDashboard() {
       >
       <iframe 
         title="Quality Dashboard"
-        src="https://drive.google.com/file/d/1wRbQsquUNety8Iter6e4AqBYCzgtvVUg/preview"
+        src="https://drive.google.com/file/d/1RsofVuvZiolmSgLtjPyRf2m0ytVejmfG/view?usp=drive_link"
         className="powerbi-container"
         allowFullScreen={true}
       />
